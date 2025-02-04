e7917d5437a1c2ee336f98cb422609da25fdd5f1
vicaon <wizvic7@gmail.com>
2025-02-04 10:25:13 +0100

    made changes to jobs.js file

--- jobs.js
+++ jobs.js
@@ -5,7 +5,6 @@ let fulljob = firstjob + " " + lastjob;
 console.log(fulljob);
 console.log(originalJob);
 
-    let scoops = 5; while (scoops > 0) {
-    console.log("Another scoop!"); scoops = scoops - 1;
-    } console.log("Life without ice cream isn't the same");
-    
+let scoops = 5; while (scoops > 0) {
+console.log("Another scoop!"); scoops = scoops - 1;
+} console.log("Life without ice cream isn't the same");
